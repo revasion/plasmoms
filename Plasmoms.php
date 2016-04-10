56251c5be4682df6e9089e698f194129c37fc8dd
Radoslav Georgiev <plasmoms@gmail.com>
2016-04-10 12:27:29 +0300

    Second commit

---
+++ Plasmoms.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace revasion\plasmoms;
+
+/**
+ * This is just an example.
+ */
+class Plasmoms extends \yii\base\Module
+{
+    public $controllerNamespace = 'revasion\plasmoms\controllers';
+
+    public function init()
+    {
+        parent::init();
+
+        /** Register custom translations for this module **/
+
+    }
+
+
+}
